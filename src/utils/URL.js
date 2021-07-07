ad55352d656fd3232fdd0a0dc7308e48a9938566
Ahmed <you@example.com>
2021-07-07 07:03:18 +0000

    url setted to heroku

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,6 +1,7 @@
 // url
 // const URL = 'http://johnsmilgatutorials.com/projects/react-tech-store-v2'
 // const URL = 'https://vintage-tech-store-react-api.herokuapp.com'
-const URL = 'http://localhost:1337'
+// const URL = 'http://localhost:1337'
+const URL = 'https://tech-store-strapi-react-api.herokuapp.com'
 
 export default URL
